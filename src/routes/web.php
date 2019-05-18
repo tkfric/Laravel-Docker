50cdfdd959ccfc5852990a1c77aef1e22f1c943b
REN FUJIE <>
2019-05-18 17:54:56 +0900

    IndexController追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+    Route::get('/misawa', 'IndexController@show');
+    Route::post('/misawa/generate','GeneratorController@execute');
+
